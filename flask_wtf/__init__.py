cc87aeed26b07f0ff5916aef0f985b550104d279
Simon Zimmermann <simonz05@gmail.com>
2012-01-24 16:09:51 +0100

    consistent naming scheme

--- flask_wtf/__init__.py
+++ flask_wtf/__init__.py
@@ -3,12 +3,12 @@ from __future__ import absolute_import
 
 from . import html5
 from .fields import *
-from .form import *
+from .forms import *
 from .validators import *
 from .widgets import *
 
 __all__  = ['wtforms.ValidationError', 'html5']
 __all__ += fields.__all__
-__all__ += form.__all__
+__all__ += forms.__all__
 __all__ += validators.__all__
 __all__ += widgets.__all__
